1ab714bf451fa577ca41d0fd898e11c82fadc9d4
cedeno <cedenojlj@gmail.com>
2021-11-23 16:55:28 -0400

    reparo archivo excel empleado

--- app/Exports/EmpleadoExport.php
+++ app/Exports/EmpleadoExport.php
@@ -4,12 +4,33 @@ namespace App\Exports;
 
 use App\Models\Empleado;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class EmpleadoExport implements FromCollection
+class EmpleadoExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
+
+    public function headings(): array
+    {
+        return [
+            'id',
+            'Nombre',
+            'Apellido',
+            'Cedula',
+            'Direccion',
+            'Telefono',
+            'Email',
+            'Salario',
+            'Foto',
+            'Empresa_id',
+            'Creado',
+            'Actualizado'
+        ];
+    }
+
+
     public function collection()
     {
         return Empleado::all();
