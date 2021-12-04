6b87049519996891a252de29332d1aa7695ddea0
cedeno <cedenojlj@gmail.com>
2021-12-04 10:31:52 -0400

    creo modelo inventario producto

---
+++ app/Http/Controllers/InvproductoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invproducto;
+use Illuminate\Http\Request;
+
+class InvproductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Invproducto  $invproducto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Invproducto $invproducto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Invproducto  $invproducto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Invproducto $invproducto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Invproducto  $invproducto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Invproducto $invproducto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Invproducto  $invproducto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Invproducto $invproducto)
+    {
+        //
+    }
+}
